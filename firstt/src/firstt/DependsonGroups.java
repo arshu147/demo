47cfb1dd4c4b2ddc9d61bb07141d553d80075ef9
madhu2810 <madhurids2892@gmail.com>
2019-02-28 20:56:50 +0530

    commit

---
+++ firstt/src/firstt/DependsonGroups.java
@@ -0,0 +1,19 @@
+package firstt;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DependsonGroups {
+	
+	@Test(dependsOnGroups="name")
+	public void run() {
+		System.out.println("running");
+	}	
+
+//	@Test(groups="name",dep)
+	public void run2() {
+		System.out.println("running2");
+	}	
+
+	
+}
