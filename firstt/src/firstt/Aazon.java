2695901b28d2fe0abb449f174c50412db8c949ab
sharmapoonam11 <sharmapoonam10001@gmail.com>
2019-03-05 15:30:03 +0530

    assign1

--- firstt/src/firstt/Aazon.java
+++ firstt/src/firstt/Aazon.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Aazon {
 
 	public static void main(String[] args) {
+		
 		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
