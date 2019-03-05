0fde0ece3a907cf19cccf12f468b9f61024c567e
madhu2810 <madhurids2892@gmail.com>
2019-03-05 22:04:50 +0530

    commit testNG

--- firstt/src/firstt/TestNGAnnotations.java
+++ firstt/src/firstt/TestNGAnnotations.java
@@ -15,12 +15,12 @@ public class TestNGAnnotations {
 	
 	@BeforeSuite
 	public void test1() {
-		Reporter.log("BeforeSuite Test1()",true);
+		Reporter.log("BeforeSuite  Test1()",true);
 	}
 	
 	@BeforeTest
 	public void test2() {
-		Reporter.log("BeforeTest Test2()",true);
+		Reporter.log("BeforeTest  Test2()",true);
 	}
 	
 	@BeforeClass
