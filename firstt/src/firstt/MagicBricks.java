769054c04b983853c494f25f13ec9895ba280e85
sharmapoonam11 <sharmapoonam10001@gmail.com>
2019-03-05 15:33:22 +0530

    ghdm

--- firstt/src/firstt/MagicBricks.java
+++ firstt/src/firstt/MagicBricks.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class MagicBricks {
 
 	public static void main(String[] args) {
+		
 		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
