e7598c5470bf6f702b2d426eddcc82bfad2128ab
Ranjith R <ranjithr.viju@gmail.com>
2019-03-05 11:20:06 +0530

    changed

--- firstt/src/firstt/MagickBricksDemo.java
+++ firstt/src/firstt/MagickBricksDemo.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-public class MagickBricks {
+public class MagickBricksDemo {
 	public static void main(String[] args) throws Exception {
 		System.setProperty("webdriver.chrome.driver", "./SeleniumServer/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
