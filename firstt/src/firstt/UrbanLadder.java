09163fda15ae6fb96e70a3737ab62ceca6f63e59
payal <patilpayal2606@gmail.com>
2019-03-06 16:53:23 +0530

    msg3

--- firstt/src/firstt/UrbanLadder.java
+++ firstt/src/firstt/UrbanLadder.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Action;
+
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -43,12 +43,9 @@ public class UrbanLadder {
 		Actions a = new Actions(driver) ;
 		// to get the menus
 
-		List<WebElement> xp = driver.findElements(By.xpath("//div[@id='topnav_wrapper']/ul/li/span[@class ='topnav_itemname']"));
+		List<WebElement> xp = driver.findElements(By.xpath("//div[@id='topnav_wrapper']/child::ul/child::li/child::span[@class ='topnav_itemname']"));
 		int count = xp.size();
 		System.out.println("Total no. of menus : "+count);
-
-
-
 		for(int i =0; i<count;i++)
 		{
 
@@ -56,32 +53,59 @@ public class UrbanLadder {
 			System.out.println("Main menu : "+menu);
 			Thread.sleep(100);
 
-		}
+			a.moveToElement(xp.get(i)).perform();
 
-		for(int i =0; i<count;i++)
+			List<WebElement> smenu = driver.findElements(By.xpath("//div[@id ='topnav_wrapper']/descendant::span[contains(@class,'topnav_itemname') and contains(.,'"+menu+"')]/following-sibling::div/descendant::div[@class='taxontype']")); 
+			int scount = smenu.size();
+			System.out.println("Submenus for "+xp.get(i).getText()+" are : ");
+
+
+			for(int j =0;j<scount;j++)
+			{
+				Thread.sleep(500);
+				String text = smenu.get(j).getText();
+				System.out.println("Submenu - "+text);
+
+			List<WebElement> list = driver.findElements(By.xpath("//div[@id='topnav_wrapper']/descendant::span[contains(@class,'topnav_itemname')and contains(.,'"+menu+"')]/following-sibling::div/descendant::div[@class='taxontype']/a[.='"+text+"']/parent::div/following-sibling::ul/descendant::a"));
+				int lcount = list.size();
+				
+				System.out.println("Options  for submenu " +text+ " are : ");
+
+				for(int k =0; k<lcount;k++)
+				{
+					Thread.sleep(500);
+					String text1 = list.get(k).getText();
+					System.out.println("      "+text1);
+				} 
+
+			}
+
+
+			/*	for(int i =0; i<count;i++)
 		{
 			a.moveToElement(xp.get(i)).perform();
-			
+
 			List<WebElement> sxp = driver.findElements(By.xpath("//div[@class='subnavlist_wrapper clearfix']/descendant::li[@class ='sublist_item']/child::div[@class='taxontype']/child::a[@class='inverted']"));
 			int scount = sxp.size(); //42
-			System.out.println("submenus for "+xp.get(i).getText()+" are : ");
+			System.out.println("Submenus for "+xp.get(i).getText()+" are : ");
 			for(int j =0; j<scount;j++)
 			{
 				Thread.sleep(500);
 				String menu1 =sxp.get(j).getText();
 				if(!menu1.isEmpty())
 				{
-				
+
 				System.out.println("        "+menu1);
-				
+
 				}
-				
+
 			}
-			
-			}
-		
-	
-}
+
+			}*/
+
+
+		}
+	}
 }
 
 
