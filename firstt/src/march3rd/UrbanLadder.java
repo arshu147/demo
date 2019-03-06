8a62f222e2eeda1959f1f209a0fce2104c8f7900
madhu2810 <madhurids2892@gmail.com>
2019-03-06 20:11:34 +0530

    commit modified u l

--- firstt/src/march3rd/UrbanLadder.java
+++ firstt/src/march3rd/UrbanLadder.java
@@ -24,31 +24,94 @@ public class UrbanLadder {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get("https://www.urbanladder.com/");
 		driver.findElement(By.xpath("//a[contains(@class,'close-reveal-modal ')]")).click();
-//		List<WebElement> allList = driver.findElements(By.xpath("//div[@id='topnav_wrapper']/ul/li/span[contains(@class,'topnav_itemname')]"));
-		List<WebElement> allList = driver.findElements(By.xpath("//div[@id='topnav_wrapper']/descendant::span[@class='topnav_itemname']")); 
-		List<WebElement> allsubList = driver.findElements(By.xpath("//div[@class='subnavlist_wrapper clearfix']/descendant::div[@class='taxontype']/a"));
 		
-		int count=allList.size();
+		List<WebElement> allList = driver.findElements(By.xpath("//div[@id='topnav_wrapper']/descendant::span[contains(@class,'topnav_itemname')]")); 
 		
-		for(int i=0;i<count;i++) {
-			String text = allList.get(i).getText();
-			System.out.println(text);
+		
+			int count=allList.size();
+//			System.out.println(count);
+		
+			for(int i=0;i<count;i++) {
+				
+			Thread.sleep(500);	
+			String text1 = allList.get(i).getText();
+			System.out.println(text1);
 			
-			Thread.sleep(1000);
 			Actions action=new Actions(driver);
-			action.moveToElement(allList.get(i)).perform();
-		
-        for (int j = 0; j < allsubList.size(); j++) {
-        	String text1 = allsubList.get(j).getText();
+			action.moveToElement(allList.get(i)).perform();		
+			Thread.sleep(500);	
+			
+			List<WebElement> allSub = driver.findElements(By.xpath("//div[@id='topnav_wrapper']/descendant::span[contains(@class,'topnav_itemname') and contains(.,'"+text1+"')]/following-sibling::div/descendant::div[@class='taxontype']/a"));
+			 
+			int count2 = allSub.size();
+//			System.out.println(count2);
+			
+			for (int j = 0; j < count2; j++)
+			{
+			Thread.sleep(500);	
+        	String text2 = allSub.get(j).getText();
+        	System.out.println(text2);
         	
-        	if(!text1.isEmpty()) {
-        		System.out.println(text1);
+        	List<WebElement> allSub2 = driver.findElements(By.xpath("//div[@id='topnav_wrapper']/descendant::span[contains(@class,'topnav_itemname') and contains(.,'"+text1+"')]/following-sibling::div/descendant::div[@class='taxontype']/a[.='"+text2+"']/parent::div/following-sibling::ul/descendant::a"));
+        	      int count3 = allSub2.size();
+        	      System.out.println(count3);
+        	    for(int k=0;k<count3;k++)    {
+        	    Thread.sleep(500);	
+        		String text3 = allSub2.get(k).getText();
+//            	System.out.println(text3);
+        		
         	}
          }		
 	  }
 		
 		driver.close();
    }
+	
+	
 }
+
+
+
+/*
+    List<WebElement> allList = driver.findElements(By.xpath("//div[@id='topnav_wrapper']/ul/li/span[conatains(.,"+Sale+")"));
+//		List<WebElement> allList = driver.findElements(By.xpath("//div[@id='topnav_wrapper']/descendant::span[@class='topnav_itemname']")); 
+//		List<WebElement> allsubList = driver.findElements(By.xpath("//div[@class='subnavlist_wrapper clearfix']/descendant::div[@class='taxontype']/a"));
+//		String s="java";
+//		System.out.println("");
+ 
+ 
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
